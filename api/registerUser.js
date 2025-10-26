8da15aef5af8579f2112c59d5ef0c6bf19510738
Martin Antonio Records <martinantoniorecords@gmail.com>
2025-10-26 22:33:29 +0200

    update

--- api/registerUser.js
+++ api/registerUser.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   try {
     const { data, error } = await supabase
-      .from("rt") // ✅ your Supabase table
+      .from("users") // ✅ your Supabase table
       .insert([{ username, email, credits: 0 }])
       .select()
       .single();
